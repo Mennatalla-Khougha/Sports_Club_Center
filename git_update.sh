5eb3948ab95fdf28bc982a52a06552acc0ebc218
George Raafat <georgexraafat@gmail.com>
2023-11-25 05:16:35 +0000

    adding console and test

--- git_update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git add .
-if [ -z "$1" ]
-then
-        git commit -m "doing some task"
-else
-        git commit -m "$1"
-fi
-git push
---
+++ test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+from models import storage
+from models.player import Player
+from models.sport import Sport
+from models.record import Record
+from models.tournament import Tournament
+from datetime import date, datetime
+
+# USER_MYSQL=admin PWD_MYSQL=admin_pwd HOST_MYSQL=localhost DB_MYSQL=club_db ./file.py
+# MYSQL_ENV=test USER_MYSQL=club_test PWD_MYSQL=club_test_pwd HOST_MYSQL=localhost DB_MYSQL=club_test_db python3 -m unittest discover tests
+
+player1 = Player(first_name='george', last_name="raafat", gender='M', birth_day=date(2004, 9, 29))
+player2 = Player(first_name='manna', last_name="k", gender='F', birth_day=date(1994, 5, 11))
+
+
+sport1 = Sport(name='tennis')
+sport2 = Sport(name='squash')
+
+tournament_tennis = Tournament(sport_id=sport1.id, name="my tournament", date=datetime(2023, 12, 15, 17, 30, 0), age_range="15-20")
+# Associate players with sports
+player1.sports.append(sport1)
+player2.sports.append(sport1)
+player2.sports.append(sport2)
+
+player1.tournaments.append(tournament_tennis)
+player2.tournaments.append(tournament_tennis)
+
+p1_sports = player1.sports
+s1_players = sport1.players
+
+records = tournament_tennis.initial_records()
+player1.save()
+player2.save()
+sport1.save()
+sport2.save()
+tournament_tennis.save()
+
+print([sport.name for sport in p1_sports])
+print([player.first_name for player in s1_players])
+print([player.first_name for player in tournament_tennis.players])
+print(tournament_tennis.date)
+print([record.to_dict() for record in records])
