4c06995171c0382f79583cbcfc89f5aa07ff6772
George Raafat <georgexraafat@gmail.com>
2023-12-04 23:37:41 +0000

    making the header responsive

---
+++ website/static/scripts/header.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".options").classList.toggle("show");
+  document.body.classList.toggle("menu-open");
+});
